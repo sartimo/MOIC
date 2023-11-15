1591b12e57c89ef3674b6e4dba7d72559c1640a2
Timo Sarkar <71646577+sartimo@users.noreply.github.com>
2023-11-15 16:30:43 +0100

    Create AssemblyInfo.cs

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Medium Orbit Ion Cannon")]
+[assembly: AssemblyDescription("TCP/IP stress-test tool")]
+[assembly: AssemblyProduct("Medium Orbit Ion Cannon")]
+[assembly: AssemblyCopyright("Public domain")]
+
+[assembly: System.Resources.NeutralResourcesLanguage("en-US")]
+
+[assembly: System.CLSCompliant(true)]
+[assembly: ComVisible(false)]
+
+[assembly: Guid("312adafc-fdac-484b-84c5-5c5457e47f67")]
+
+[assembly: AssemblyVersion("2.9.9.99")]
+[assembly: AssemblyFileVersionAttribute("2.9.9.99")]
